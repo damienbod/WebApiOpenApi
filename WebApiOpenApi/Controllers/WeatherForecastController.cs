0ca0e8852820422c35629d1ed68aafae6ee4334f
damienbod <damien_bod@hotmail.com>
2024-08-02 09:04:19 +0200

    Add Open API transformer

--- WebApiOpenApi/Controllers/WeatherForecastController.cs
+++ WebApiOpenApi/Controllers/WeatherForecastController.cs
@@ -34,21 +34,21 @@ namespace WebApiOpenApi.Controllers
             .ToArray();
         }
 
-        [EndpointSummary("This is a second summary from OpenApi attributes.")]
-        [EndpointDescription("This is a second description from OpenApi attributes.")]
-        [Produces(typeof(IEnumerable<WeatherForecast>))]
-        [HttpGet(Name = "GetWeatherForecastWithParameter")]
-        public IEnumerable<WeatherForecast> GetWithParameter(
-            [Description("parameter name description using OpenApi")] string name)
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+        //[EndpointSummary("This is a second summary from OpenApi attributes.")]
+        //[EndpointDescription("This is a second description from OpenApi attributes.")]
+        //[Produces(typeof(IEnumerable<WeatherForecast>))]
+        //[HttpGet(Name = "GetWeatherForecastWithParameter")]
+        //public IEnumerable<WeatherForecast> GetWithParameter(
+        //    [Description("parameter name description using OpenApi")] string name)
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
         
     }
 }
--- WebApiOpenApi/Program.cs
+++ WebApiOpenApi/Program.cs
@@ -4,7 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddOpenApi("v1"); 
+builder.Services.AddOpenApi(options =>
+{
+    options.UseTransformer((document, context, cancellationToken) =>
+    {
+        document.Info = new()
+        {
+            Title = "My API",
+            Version = "v1",
+            Description = "API for Damien"
+        };
+        return Task.CompletedTask;
+    });
+});
 
 var app = builder.Build();
 
