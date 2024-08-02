b068b820a7d8e67fcd370ba8d172a5b3e2f651f4
damienbod <damien_bod@hotmail.com>
2024-08-02 10:35:21 +0200

    Update endpoints

--- WebApiOpenApi/Controllers/WeatherForecastController.cs
+++ WebApiOpenApi/Controllers/WeatherForecastController.cs
@@ -22,7 +22,7 @@ namespace WebApiOpenApi.Controllers
         [EndpointSummary("This is a summary from OpenApi attributes.")]
         [EndpointDescription("This is a description from OpenApi attributes.")]
         [Produces(typeof(IEnumerable<WeatherForecast>))]
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet("GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -34,21 +34,20 @@ namespace WebApiOpenApi.Controllers
             .ToArray();
         }
 
-        //[EndpointSummary("This is a second summary from OpenApi attributes.")]
-        //[EndpointDescription("This is a second description from OpenApi attributes.")]
-        //[Produces(typeof(IEnumerable<WeatherForecast>))]
-        //[HttpGet(Name = "GetWeatherForecastWithParameter")]
-        //public IEnumerable<WeatherForecast> GetWithParameter(
-        //    [Description("parameter name description using OpenApi")] string name)
-        //{
-        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        //        TemperatureC = Random.Shared.Next(-20, 55),
-        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        //    })
-        //    .ToArray();
-        //}
-        
+        [EndpointSummary("This is a second summary from OpenApi attributes.")]
+        [EndpointDescription("This is a second description from OpenApi attributes.")]
+        [Produces(typeof(IEnumerable<WeatherForecast>))]
+        [HttpGet("GetWeatherForecastWithParameter")]
+        public IEnumerable<WeatherForecast> GetWithParameter(
+            [Description("parameter name description using OpenApi")] string name)
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
