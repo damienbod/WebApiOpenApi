6f180b546b00e6c58f4108c8c5c23070b9550dbd
damienbod <damien_bod@hotmail.com>
2024-08-02 08:25:27 +0200

    Add some openapi attributes

--- WebApiOpenApi/Controllers/WeatherForecastController.cs
+++ WebApiOpenApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 
 namespace WebApiOpenApi.Controllers
 {
@@ -18,6 +19,9 @@ namespace WebApiOpenApi.Controllers
             _logger = logger;
         }
 
+        [EndpointSummary("This is a summary from OpenApi attributes.")]
+        [EndpointDescription("This is a description from OpenApi attributes.")]
+        [Produces(typeof(IEnumerable<WeatherForecast>))]
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
@@ -29,5 +33,22 @@ namespace WebApiOpenApi.Controllers
             })
             .ToArray();
         }
+
+        [EndpointSummary("This is a second summary from OpenApi attributes.")]
+        [EndpointDescription("This is a second description from OpenApi attributes.")]
+        [Produces(typeof(IEnumerable<WeatherForecast>))]
+        [HttpGet(Name = "GetWeatherForecastWithParameter")]
+        public IEnumerable<WeatherForecast> GetWithParameter(
+            [Description("parameter name description using OpenApi")] string name)
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        
     }
 }
