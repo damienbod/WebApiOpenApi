0502a4282cec271c78e3c276c578e90c93224479
damienbod <damien_bod@hotmail.com>
2024-08-03 08:20:29 +0200

    add some definitions

--- WebApiOpenApi/Controllers/WeatherForecastController.cs
+++ WebApiOpenApi/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 
 namespace WebApiOpenApi.Controllers;
 
+[Authorize(AuthenticationSchemes = "Bearer")]
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController(ILogger<WeatherForecastController> _logger) : ControllerBase
@@ -18,23 +19,24 @@ public class WeatherForecastController(ILogger<WeatherForecastController> _logge
     [EndpointDescription("This is a description from OpenApi attributes.")]
     [Produces(typeof(IEnumerable<WeatherForecast>))]
     [HttpGet("GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public IActionResult Get()
     {
         _logger.LogDebug("GetWeatherForecast with OpenAPI definitions");
 
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
-        .ToArray();
+        .ToArray());
     }
 
-    [Authorize(AuthenticationSchemes = "Bearer")]
+
     [EndpointSummary("This is a second summary from OpenApi attributes.")]
     [EndpointDescription("This is a second description from OpenApi attributes.")]
     [Produces(typeof(IEnumerable<WeatherForecast>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpGet("GetWeatherForecastWithParameter")]
     public IEnumerable<WeatherForecast> GetWithParameter(
         [Description("parameter name description using OpenApi")] string name)
@@ -46,5 +48,24 @@ public class WeatherForecastController(ILogger<WeatherForecastController> _logge
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
+    }
+
+    [EndpointSummary("This is a second summary from OpenApi attributes.")]
+    [EndpointDescription("This is a second description from OpenApi attributes.")]
+    [HttpPut("PutWeatherForecast")]
+    public IActionResult PutWeatherForecast(
+        [Description("parameter put item using OpenApi")] WeatherForecast weatherForecast)
+    {
+        return Created();
+    }
+
+    [EndpointSummary("This is a second summary from OpenApi attributes.")]
+    [EndpointDescription("This is a second description from OpenApi attributes.")]
+    [Produces(typeof(IEnumerable<WeatherForecast>))]
+    [HttpPost("PostWeatherForecast")]
+    public IActionResult PostWeatherForecast(
+        [Description("parameter post item using OpenApi")] WeatherForecast weatherForecast)
+    {
+        return Ok(weatherForecast);
     }
 }
