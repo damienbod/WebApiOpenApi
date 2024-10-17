19d809ce94fb6604ba0e5abb9e2dbd5f261af213
damienbod <damien_bod@hotmail.com>
2024-10-17 19:12:22 +0200

    Update security headers definitions

---
+++ WebApiOpenApi/SecurityHeadersDefinitionsAPI.cs
@@ -0,0 +1,32 @@
+﻿namespace WebApiOpenApi;
+
+public static class SecurityHeadersDefinitionsAPI
+{
+    public static HeaderPolicyCollection GetHeaderPolicyCollection()
+    {
+        var policy = new HeaderPolicyCollection()
+            .AddFrameOptionsDeny()
+            .AddContentTypeOptionsNoSniff()
+            .AddReferrerPolicyStrictOriginWhenCrossOrigin()
+            .AddCrossOriginOpenerPolicy(builder => builder.SameOrigin())
+            .AddCrossOriginEmbedderPolicy(builder => builder.RequireCorp())
+            .AddCrossOriginResourcePolicy(builder => builder.SameOrigin())
+            .RemoveServerHeader()
+            .AddPermissionsPolicyWithDefaultSecureDirectives();
+
+        policy.AddContentSecurityPolicy(builder =>
+        {
+            builder.AddObjectSrc().None();
+            builder.AddBlockAllMixedContent();
+            builder.AddImgSrc().Self().From("data:");
+            builder.AddFormAction().Self();
+            builder.AddFontSrc().Self();
+            builder.AddStyleSrc().Self().UnsafeInline();
+            builder.AddScriptSrc().Self().UnsafeInline(); //.WithNonce();
+            builder.AddBaseUri().Self();
+            builder.AddFrameAncestors().None();
+        });
+
+        return policy;
+    }
+}
---
+++ WebApiOpenApi/SecurityHeadersDefinitionsSwagger.cs
@@ -0,0 +1,39 @@
+﻿namespace WebApiOpenApi;
+
+public static class SecurityHeadersDefinitionsSwagger
+{
+    public static HeaderPolicyCollection GetHeaderPolicyCollection(bool isDev)
+    {
+        var policy = new HeaderPolicyCollection()
+            .AddFrameOptionsDeny()
+            .AddContentTypeOptionsNoSniff()
+            .AddReferrerPolicyStrictOriginWhenCrossOrigin()
+            .AddCrossOriginOpenerPolicy(builder => builder.SameOrigin())
+            .AddCrossOriginEmbedderPolicy(builder => builder.RequireCorp())
+            .AddCrossOriginResourcePolicy(builder => builder.SameOrigin())
+            .RemoveServerHeader()
+            .AddPermissionsPolicyWithDefaultSecureDirectives();
+
+        policy.AddContentSecurityPolicy(builder =>
+        {
+            builder.AddObjectSrc().None();
+            builder.AddBlockAllMixedContent();
+            builder.AddImgSrc().None();
+            builder.AddFormAction().None();
+            builder.AddFontSrc().None();
+            builder.AddStyleSrc().None();
+            builder.AddScriptSrc().None();
+            builder.AddBaseUri().Self();
+            builder.AddFrameAncestors().None();
+            builder.AddCustomDirective("require-trusted-types-for", "'script'");
+        });
+
+        if (!isDev)
+        {
+            // maxage = one year in seconds
+            policy.AddStrictTransportSecurityMaxAgeIncludeSubDomains(maxAgeInSeconds: 60 * 60 * 24 * 365);
+        }
+
+        return policy;
+    }
+}
